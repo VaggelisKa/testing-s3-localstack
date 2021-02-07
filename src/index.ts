f9ac43615565a24ddfd7294860e8506e7c10c2b7
Vaggelis Karavasileiadis <e.karavasileiadis@gmail.com>
2021-02-07 19:17:37 +0200

    Setting up CORS

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,16 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+      'Access-Control-Allow-Headers', 
+      'Origin, X-Requested-Width, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  next();
+});
+
 app.use('/api/documents', routes);
 
 app.listen(8000, async () => {
